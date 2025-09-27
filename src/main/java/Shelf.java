6a5299bf74ba4dff91ff6461108e81a940b4336d
ccupa <ccupa@csumb.edu>
2025-09-27 15:29:56 -0700

    Shelf.java commit: test 2 pass. Fixes in addBook

--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -72,37 +72,38 @@ public class Shelf {
     public Code addBook(Book book) {
         if (books.containsKey(book)) {
             int count = books.get(book);
-            books.put(book, count++);
-            System.out.println(book.getTitle() + " added to shelf " + this.subject);
-            return Code.SUCCESS;
-        }
-        if (book.getSubject().equals(this.subject)) {
-            books.put(book, 1);
-            System.out.println(book.getTitle()+ " added to shelf " + this.subject);
+            books.put(book, count + 1);
+            System.out.println(book.toString() + " added to shelf " + this.toString());
             return Code.SUCCESS;
         } else {
-            return Code.SHELF_SUBJECT_MISMATCH_ERROR;
+            if (book.getSubject().equals(this.subject)) {
+                books.put(book, 1);
+                System.out.println(book.toString()+ " added to shelf " + this.toString());
+                return Code.SUCCESS;
+            } else {
+                return Code.SHELF_SUBJECT_MISMATCH_ERROR;
+            }
         }
     }
 
-    public Code removeBook(Book book) {
-        if (!books.containsKey(book)) {
-            System.out.println(book.getTitle() + " is not on shelf " + this.subject);
-            return Code.BOOK_NOT_IN_INVENTORY_ERROR;
-        } else if (books.containsKey(book) && books.get(book) == 0) {
-            System.out.println("No copies of " + book.getTitle() + " remain on the shelf " + this.subject);
-            return Code.BOOK_NOT_IN_INVENTORY_ERROR;
-        } else if (books.containsKey(book) && books.get(book) > 0) {
-            books.put(book, books.get(book) - 1);
-            System.out.println(book.getTitle() + " successfully removed from shelf " + this.subject);
-            return Code.SUCCESS;
-        } else {
-            return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+        public Code removeBook (Book book){
+            if (!books.containsKey(book)) {
+                System.out.println(book.getTitle() + " is not on shelf " + this.subject);
+                return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+            } else if (books.containsKey(book) && books.get(book) == 0) {
+                System.out.println("No copies of " + book.getTitle() + " remain on the shelf " + this.subject);
+                return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+            } else if (books.containsKey(book) && books.get(book) > 0) {
+                books.put(book, books.get(book) - 1);
+                System.out.println(book.getTitle() + " successfully removed from shelf " + this.subject);
+                return Code.SUCCESS;
+            } else {
+                return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+            }
         }
-    }
 
-    public String listBooks(){
-        int totalBooks = 0;
+        public String listBooks () {
+            int totalBooks = 0;
             for (int count : books.values()) {
                 totalBooks += count;
             }
@@ -116,6 +117,6 @@ public class Shelf {
                         .append(" ISBN:").append(book.getIsbn()).append(" ")
                         .append(count).append("\n");
             }
-        return sb.toString();
+            return sb.toString();
+        }
     }
-}
